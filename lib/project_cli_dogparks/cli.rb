c4877e183668c206b0f47d792ad469a302717c1b
Jon <jon_p_stone@yahoo.com>
2017-01-27 04:18:01 -0600

    CLI class development p1.

--- lib/project_cli_dogparks/cli.rb
+++ lib/project_cli_dogparks/cli.rb
@@ -3,6 +3,7 @@ class ProjectCliDogparks::CLI
   def call
     list_parks
     choice
+    so_long
   end
 
   def list_parks
@@ -15,7 +16,23 @@ class ProjectCliDogparks::CLI
   end
 
   def choice
-    puts "Please make a selection..."
+    input = nil
+    while input != "exit"
+      puts "Please make a selection or type 'exit'"
+      input = gets.strip.downcase
+      case input
+      when "1"
+        puts "blah blah"
+      when "2"
+        puts "blah blah blah"
+      when "3"
+        puts "blah blah blah blah"
+      end
+    end
+  end
+
+  def so_long
+    puts "Have fun walking your dog(s)!"
   end
 
 end
